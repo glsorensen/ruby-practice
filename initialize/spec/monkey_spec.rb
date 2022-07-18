aff8d496449237e84cf87731fdf34c3a26f5b7f0
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-07-18 09:00:35 -0600

    Monkey Complete

--- initialize/spec/monkey_spec.rb
+++ initialize/spec/monkey_spec.rb
@@ -2,25 +2,25 @@ require 'rspec'
 require_relative '../lib/monkey'
 
 RSpec.describe Monkey do
-  xit 'exists' do
+  it 'exists' do
     margaret = Monkey.new(["Margaret", "Bonobo", "Bananas"])
 
     expect(margaret).to be_an_instance_of(Monkey)
   end
 
-  xit 'has a name' do
+  it 'has a name' do
     margaret = Monkey.new(["Margaret", "Bonobo", "Bananas"])
 
     expect(margaret.name).to eq("Margaret")
   end
 
-  xit 'has a type' do
+  it 'has a type' do
     margaret = Monkey.new(["Margaret", "Bonobo", "Bananas"])
 
     expect(margaret.type).to eq("Bonobo")
   end
 
-  xit 'has a favorite_food' do
+  it 'has a favorite_food' do
     margaret = Monkey.new(["Margaret", "Bonobo", "Bananas"])
 
     expect(margaret.favorite_food).to eq("Bananas")
