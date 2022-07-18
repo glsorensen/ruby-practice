a5c85d35b3fc1b2dcb373c8b2a210fa523fca2ec
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-07-18 08:49:24 -0600

    Jackalope Complete

--- initialize/spec/jackalope_spec.rb
+++ initialize/spec/jackalope_spec.rb
@@ -2,19 +2,19 @@ require 'rspec'
 require_relative '../lib/jackalope'
 
 RSpec.describe Jackalope do
-  xit 'exists' do
+  it 'exists' do
     jorge = Jackalope.new("Jorge")
 
     expect(jorge).to be_an_instance_of(Jackalope)
   end
 
-  xit 'has a name' do
+  it 'has a name' do
     jorge = Jackalope.new("Jorge")
 
     expect(jorge.name).to eq("Jorge")
   end
 
-  xit 'starts with an array of etymology' do
+  it 'starts with an array of etymology' do
     jorge = Jackalope.new("Jorge")
 
     expect(jorge.etymology).to eq(["Jackrabbit", "Antelope"])
