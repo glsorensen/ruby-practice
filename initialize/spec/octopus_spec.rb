3e7abc6094c55b4eb747260c3ddb842a84d366b9
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-07-18 09:10:23 -0600

    Octopus Complete

--- initialize/spec/octopus_spec.rb
+++ initialize/spec/octopus_spec.rb
@@ -4,30 +4,30 @@ require_relative '../lib/narwhal'
 require_relative '../lib/octopus'
 
 RSpec.describe Octopus do
-  xit 'exists' do
+  it 'exists' do
     nancy = Narwhal.new({cute: true, weight: 500, name: "Nancy"})
     orville = Octopus.new("Orville", nancy)
 
     expect(orville).to be_an_instance_of(Octopus)
   end
 
-  xit 'has a name' do
+  it 'has a name' do
     nancy = Narwhal.new({cute: true, weight: 500, name: "Nancy"})
     orville = Octopus.new("Orville", nancy)
 
     expect(orville.name).to eq("Orville")
   end
 
-  xit 'has an animal as a friend' do
+  it 'has an animal as a friend' do
     nancy = Narwhal.new({cute: true, weight: 500, name: "Nancy"})
     orville = Octopus.new("Orville", nancy)
-
+    
     expect(orville.friend).to eq(nancy)
   end
 
-  xit 'can have any animal as a friend' do
+  it 'can have any animal as a friend' do
     eel = Eel.new("Earl")
-    orville = Octopus.new("Orville", nancy)
+    orville = Octopus.new("Orville", eel)
 
     expect(orville.friend).to eq(eel)
   end
