520a89120bf52a3c58a7fdbc8ea8ea01ffd85d97
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-07-18 08:51:28 -0600

    Kangaroo Complete

--- initialize/spec/kangaroo_spec.rb
+++ initialize/spec/kangaroo_spec.rb
@@ -2,13 +2,13 @@ require 'rspec'
 require_relative '../lib/kangaroo'
 
 RSpec.describe Kangaroo do
-  xit 'exists' do
+  it 'exists' do
     kerrie = Kangaroo.new("Kerrie", ["Kat", "Kyle"])
 
     expect(kerrie).to be_an_instance_of(Kangaroo)
   end
 
-  xit 'has children' do
+  it 'has children' do
     kerrie = Kangaroo.new("Kerrie", ["Kat", "Kyle"])
 
     expect(kerrie.children).to eq(["Kat", "Kyle"])
