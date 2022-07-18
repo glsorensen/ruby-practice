0575dbda041c07959180e65ccfc09b64ab0aa146
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-07-18 09:14:27 -0600

    Parrot Complete

--- initialize/spec/parrot_spec.rb
+++ initialize/spec/parrot_spec.rb
@@ -2,28 +2,28 @@ require 'rspec'
 require_relative '../lib/parrot'
 
 RSpec.describe Parrot do
-  xit 'exists' do
+  it 'exists' do
     words = ["Sugar", "Flashy", "Giant", "Whisper"]
     perry = Parrot.new({name: "Perry", known_words: words})
 
     expect(perry).to be_an_instance_of(Parrot)
   end
 
-  xit 'has a name' do
+  it 'has a name' do
     words = ["Sugar", "Flashy", "Giant", "Whisper"]
     perry = Parrot.new({name: "Perry", known_words: words})
 
     expect(perry.name).to eq("Perry")
   end
 
-  xit 'has a sound' do
+  it 'has a sound' do
     words = ["Sugar", "Flashy", "Giant", "Whisper"]
     perry = Parrot.new({name: "Perry", known_words: words})
 
     expect(perry.sound).to eq('Squawk!')
   end
 
-  xit 'knows words' do
+  it 'knows words' do
     words = ["Sugar", "Flashy", "Giant", "Whisper"]
     perry = Parrot.new({name: "Perry", known_words: words})
 
