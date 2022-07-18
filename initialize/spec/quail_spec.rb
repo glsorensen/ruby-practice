a7ba265790f8bea3443cf8a85cbfc142297dc0e6
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-07-18 09:16:37 -0600

    Quail Complete

--- initialize/spec/quail_spec.rb
+++ initialize/spec/quail_spec.rb
@@ -2,19 +2,19 @@ require 'rspec'
 require_relative '../lib/quail'
 
 RSpec.describe Quail do
-  xit 'exists' do
+  it 'exists' do
     quinn = Quail.new("Quinn")
 
     expect(quinn).to be_an_instance_of(Quail)
   end
 
-  xit 'has a name' do
+  it 'has a name' do
     quinn = Quail.new("Quinn")
 
     expect(quinn.name).to eq("Quinn")
   end
 
-  xit 'will capitalize its name' do
+  it 'will capitalize its name' do
     quinn = Quail.new("quinn")
 
     expect(quinn.name).to eq("Quinn")
